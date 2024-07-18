1d54c0adc0772d3e87a48da71380818eafa28e7e
Lucas <lucascostamonteiro@yahoo.com>
2024-07-18 10:08:28 -0700

    project skeleton

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+// backend/index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
